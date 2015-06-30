b41a654ef84db73b7bf9b649220d3e6ce816a4aa
Jonathan Knight <jonny.mongrel@gmail.com>
2015-06-30 09:38:23 +0100

    Updates to structure and retain values when changing type

--- js/app.js
+++ js/app.js
@@ -31,18 +31,24 @@ requirejs(['jquery', 'app/data', 'jquery-ui'],
        */
       _create: function() {
         var self = this,
-            O    = self.options,
-            E    = self.element,
-            D    = JSON.parse(self.element.val());
+            data = JSON.parse(self.element.val());
 
-        self._data = new VisualData('myObjectInput', D);
+        self.options = $.extend(self.options, self.data_options(self.element));
+        self._data   = new VisualData('myObjectInput', data);
+        window.console && console.log(self.options);
         /**
          * DOM Element to represent the editor
          * @type {*|jQuery|HTMLElement}
          */
         self._E = {};
-        self._E.editor = $('<div/>').bind({
+        self._E.container = $('<div/>', {class: 'schema-editor'});
+        self._E.editor    = $('<div/>', {class: 'view-port'}).bind({
           'click' : function(e) {self._handleClick(e)}
+        });
+        $(document.body).on('schema-change', function () {
+          window.console && console.log('schema change');
+          var data = VisualData.toData(self._E.editor.find('>.data-value'));
+          self.element.val(JSON.stringify(data,null,'\t'));
         });
         self._E.controls = $('<div/>');
       },
@@ -54,11 +60,11 @@ requirejs(['jquery', 'app/data', 'jquery-ui'],
         self._E.editor.append(this._data.draw());
         //this.element.css({display: 'none'}).after(this._E.editor);
         self.element
-          .on('click', function () {
-            var data = VisualData.toData(self._E.editor.find('>.data-value'));
-            self.element.val(JSON.stringify(data));
-          })
-          .after(this._E.editor);
+          .wrap(self._E.container)
+          .before(this._E.editor);
+        if (!self.options['show-output']) {
+          self.element.hide();
+        }
       },
       /**
        *
@@ -233,21 +239,89 @@ requirejs(['jquery', 'app/data', 'jquery-ui'],
       _uiTypeDropdown : function() {
         var select = $('<select/>'),
           types  = {
-            'null'   : 'Null',
+            'string' : 'Text',
             'object' : 'Object',
             'array'  : 'Collection',
             'number' : 'Number',
-            'string' : 'Text',
-            'boolean': 'Bool'
+            'boolean': 'Bool',
+            'null'   : 'Null'
           };
         $.each(types, function(value, label) {
           select.append($('<option/>',{value: value, text: label}));
         });
         return select;
+      },
+
+      // Description:
+      //    Parses data-options attribute
+      //
+      // Arguments:
+      //    El (jQuery Object): Element to be parsed.
+      //
+      // Returns:
+      //    Options (Javascript Object): Contents of the element's data-options
+      //    attribute.
+      data_options : function (el, data_attr_name) {
+        data_attr_name = data_attr_name || 'options';
+        var opts = {}, ii, p, opts_arr,
+          data_options = function (el) {
+            var namespace = '';
+
+            if (namespace.length > 0) {
+              return el.data(namespace + '-' + data_attr_name);
+            }
+
+            return el.data(data_attr_name);
+          };
+
+        var cached_options = data_options(el);
+
+        if (typeof cached_options === 'object') {
+          return cached_options;
+        }
+
+        opts_arr = (cached_options || ':').split(';');
+        ii = opts_arr.length;
+
+        function isNumber (o) {
+          return !isNaN (o - 0) && o !== null && o !== '' && o !== false && o !== true;
+        }
+
+        function trim (str) {
+          if (typeof str === 'string') {
+            return $.trim(str);
+          }
+          return str;
+        }
+
+        while (ii--) {
+          p = opts_arr[ii].split(':');
+          p = [p[0], p.slice(1).join(':')];
+
+          if (/true/i.test(p[1])) {
+            p[1] = true;
+          }
+          if (/false/i.test(p[1])) {
+            p[1] = false;
+          }
+          if (isNumber(p[1])) {
+            if (p[1].indexOf('.') === -1) {
+              p[1] = parseInt(p[1], 10);
+            } else {
+              p[1] = parseFloat(p[1]);
+            }
+          }
+
+          if (p.length === 2 && p[0].length > 0) {
+            opts[trim(p[0])] = trim(p[1]);
+          }
+        }
+
+        return opts;
       }
     });
 
-    $('[data-ui-objecteditor]').each(function () {
+    $('[data-schema-editor]').each(function () {
       var editor = $(this).objectEditor({some : 'option'});
       editor.objectEditor('show');
     });
